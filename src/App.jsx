5965665f5494a6326b46eb5b64b9887470b34948
SudeeptoBose <sudeeptobose23@gmail.com>
2024-07-01 19:19:23 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from "@react-three/fiber"
+import Experience from "./Experience"
+
+
+function App() {
+
+  return (
+    <>
+      <Canvas>
+        <Experience/>
+      </Canvas>
+    </>
+  )
+}
+
+export default App
---
+++ src/Experience.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TVScene } from './Tv-scene'
+
+const Experience = () => {
+  return (
+    <>
+        <TVScene/>
+    </>
+  )
+}
+
+export default Experience
